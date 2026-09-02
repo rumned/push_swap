0a24dfbb3bb1577805935f4e3cc30ddc98c947e6
Mohammad Ramdzuanny Bin Musram Rakunman <mbin-mus@c1r7s2.42penang.edu.my>
2026-09-02 21:58:42 +0800

    changed back sort_stack to have 3 conditions based on disorder, needs chunk sort done to compile again

--- sort_stack.c
+++ sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: mbin-mus <mbin-mus@student.42penang.edu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 19:22:17 by mbin-mus          #+#    #+#             */
-/*   Updated: 2026/09/02 21:53:16 by mbin-mus         ###   ########.fr       */
+/*   Updated: 2026/09/02 21:57:16 by mbin-mus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	sort_stack(t_list **a, t_list **b, t_bench *bench, t_ops *operation)
 	disorder = bench->disorder;
 	if (disorder < 0.2)
 		insertion_sort(a, b, operation);
-	// else if (disorder >= 0.2 && disorder < 0.5)
-	// 	chunk_sort(a, b, &operation);
-	// else if (disorder >= 0.5)
-	else
+	else if (disorder >= 0.2 && disorder < 0.5)
+		chunk_sort(a, b, &operation);
+	else if (disorder >= 0.5)
+	// else
 		radix_base4(a, b, operation);
 }
