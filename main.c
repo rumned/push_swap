0807602f6303963f4d3e9c7c0488f5c08168d8e9
Mohammad Ramdzuanny Bin Musram Rakunman <mbin-mus@c1r7s3.42penang.edu.my>
2026-09-05 00:04:03 +0800

    tested if still compilable, changed comment type in sort_stack and in main, so that checker could be used

--- main.c
+++ main.c
@@ -6,53 +6,53 @@
 /*   By: mbin-mus <mbin-mus@student.42penang.edu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 16:42:53 by mbin-mus          #+#    #+#             */
-/*   Updated: 2026/09/04 20:48:10 by mbin-mus         ###   ########.fr       */
+/*   Updated: 2026/09/05 00:00:26 by mbin-mus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	print_op(char *name, int count)
-{
-	int	i;
+// static void	print_op(char *name, int count)
+// {
+// 	int	i;
 
-	i = 0;
-	while (name[i])
-		ft_putchar_fd(name[i++], 2);
-	ft_putchar_fd(':', 2);
-	ft_putchar_fd(' ', 2);
-	ft_putnbr_fd(count, 2);
-	ft_putchar_fd('\n', 2);
-}
+// 	i = 0;
+// 	while (name[i])
+// 		ft_putchar_fd(name[i++], 2);
+// 	ft_putchar_fd(':', 2);
+// 	ft_putchar_fd(' ', 2);
+// 	ft_putnbr_fd(count, 2);
+// 	ft_putchar_fd('\n', 2);
+// }
 
-static void	print_ops(t_ops *operation)
-{
-	print_op("sa", operation->sa);
-	print_op("sb", operation->sb);
-	print_op("ss", operation->ss);
-	print_op("pa", operation->pa);
-	print_op("pb", operation->pb);
-	print_op("ra", operation->ra);
-	print_op("rb", operation->rb);
-	print_op("rr", operation->rr);
-	print_op("rra", operation->rra);
-	print_op("rrb", operation->rrb);
-	print_op("rrr", operation->rrr);
-	print_op("total", operation->total);
-}
+// static void	print_ops(t_ops *operation)
+// {
+// 	print_op("sa", operation->sa);
+// 	print_op("sb", operation->sb);
+// 	print_op("ss", operation->ss);
+// 	print_op("pa", operation->pa);
+// 	print_op("pb", operation->pb);
+// 	print_op("ra", operation->ra);
+// 	print_op("rb", operation->rb);
+// 	print_op("rr", operation->rr);
+// 	print_op("rra", operation->rra);
+// 	print_op("rrb", operation->rrb);
+// 	print_op("rrr", operation->rrr);
+// 	print_op("total", operation->total);
+// }
 
-static void	print_list(t_list *head)
-{
-	t_list	*tmp;
+// static void	print_list(t_list *head)
+// {
+// 	t_list	*tmp;
 
-	tmp = head;
-	while (tmp != NULL)
-	{
-		ft_putnbr_fd(tmp->value, 1);
-		ft_putendl_fd("", 1);
-		tmp = tmp->next;
-	}
-}
+// 	tmp = head;
+// 	while (tmp != NULL)
+// 	{
+// 		ft_putnbr_fd(tmp->value, 1);
+// 		ft_putendl_fd("", 1);
+// 		tmp = tmp->next;
+// 	}
+// }
 
 static	void	free_stacks(t_list **stack_a, t_list **stack_b)
 {
@@ -83,8 +83,8 @@ int	main(int ac, char **av)
 	}
 	bench = calc_disorder(*stack_a);
 	sort_stack(stack_a, stack_b, &bench, &operation);
-	print_list(*stack_a);
-	print_ops(&operation);
+	// print_list(*stack_a);
+	// print_ops(&operation);
 	free_stacks(stack_a, stack_b);
 	return (0);
 }
