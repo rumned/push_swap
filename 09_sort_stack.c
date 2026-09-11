d0ce36fb512f90d7d4caa0af44755c5c5bd81358
Nikki Kai Xuan Sim <nisim@c1r3s5.42penang.edu.my>
2026-09-11 18:17:55 +0800

    update bench info & print

--- 09_sort_stack.c
+++ 09_sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: mbin-mus <mbin-mus@student.42penang.edu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 19:22:17 by mbin-mus          #+#    #+#             */
-/*   Updated: 2026/09/11 17:24:13 by nisim            ###   ########.fr       */
+/*   Updated: 2026/09/11 18:16:18 by nisim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 static void	set_label(t_bench *bench)
 {
-	static char *const	names[] = {"adaptive", "Insertion sort",
-		"Chunk sort", "Quick sort", "adaptive"};
-	static char *const	classes[] = {"adaptive", "O(n²)",
-		"O(n√n))", "O(n log n)", "adaptive"};
+	static char *const	names[] = {"Adaptive", "Insertion sort",
+		"Chunk sort", "Quick sort", "Adaptive"};
+	static char *const	classes[] = {"Adaptive", "O(n²)",
+		"O(n√n))", "O(n log n)", "Adaptive"};
 
 	if (bench->mode > ADAPTIVE)
 		bench->mode = DEFAULT;
@@ -31,11 +31,20 @@ void	adaptive_sort(t_list **a, t_list **b, t_bench *bench, t_ops *ops)
 
 	disorder = bench->disorder;
 	if (disorder < 0.2)
+	{
 		insertion_sort(a, b, ops);
+		bench->complex_class = "O(n²)";
+	}
 	else if (disorder >= 0.2 && disorder < 0.5)
+	{
 		chunk_sort(a, b, ops);
+		bench->complex_class = "O(n√n)";
+	}
 	else if (disorder >= 0.5)
+	{
 		dual_quicksort(a, b, ops);
+		bench->complex_class = "O(n log n)";
+	}
 }
 
 void	sort_stack(t_list **a, t_list **b, t_bench *bench, t_ops *ops)
